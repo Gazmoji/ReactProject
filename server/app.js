323593291249c7bd6b4b530648da61bfa287e978
root <root@DESKTOP-FMI1SH6>
2023-06-04 23:16:00 -0400

    changes

--- server/app.js
+++ server/app.js
@@ -67,7 +67,7 @@ app.post("/login", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../dist/index.html"));
 });
 
 app.listen(port, () => {
