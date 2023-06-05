06571d23ebd37ec11e98fdb42c1b771df92460f9
Axel L <axel@firma.finance>
2023-06-04 21:20:29 -0700

    fix: removed bad added good

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "../public")));
+app.use(express.static(path.resolve(__dirname, "../dist")));
 
 const port = process.env.PORT || 3000;
 
@@ -67,7 +67,7 @@ app.post("/login", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../public/index.html"));
+  res.sendFile(path.resolve(__dirname, "../dist/index.html"));
 });
 
 app.listen(port, () => {
