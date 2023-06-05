e8518ec117b1e9e145ec2f1baed27afc070d2d5f
root <root@DESKTOP-FMI1SH6>
2023-06-04 23:33:10 -0400

    changes

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.resolve(__dirname, "../dist")));
 
 const port = process.env.PORT || 3000;
 
